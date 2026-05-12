f3ab75555992e362d80943c43f6606fc00fec59d
Adouni Islem <islem.adouni18@gmail.com>
2026-05-12 18:58:11 +0100

    Initial commit

---
+++ debo-app/src/main/java/com/debo/debo_app/backend/DeboAppApplication.java
@@ -0,0 +1,13 @@
+package com.debo.debo_app.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeboAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeboAppApplication.class, args);
+	}
+
+}
---
+++ debo-app/src/test/java/com/debo/debo_app/backend/DeboAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debo.debo_app.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeboAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
